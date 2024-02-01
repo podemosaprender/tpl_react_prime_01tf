31f22524130b93fbc4089e29fb0de322bc1b4605
devel@mauriciocap.com <devel@mauriciocap.com>
2024-02-01 11:29:40 -0500

    TRY: ejecutar en glitch.com

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const BasePath= (process.env.GITHUB_REPOSITORY||'').replace(/^[^\/]*/,'') 
+const esGlitch= (process.env.API_SERVER_EXTERNAL!=null)
 
 //SEE: https://vitejs.dev/config/
-export default defineConfig({
+const config= defineConfig({
 	base: BasePath,
   plugins: [react()],
 })
+
+if (esGlitch) {
+  config.server= {
+    host:"0.0.0.0",
+    port:3000,
+    strictPort: true,
+    hmr: {
+      clientPort: 443 // Run the websocket server on the SSL port
+    }
+  }
+}
+
+export default config;
+
